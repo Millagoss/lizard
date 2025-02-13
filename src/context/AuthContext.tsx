d5a96b1585fc1409767f2be53485ade22471395e
millagoss <millagoss19@gmail.com>
2025-02-13 17:08:41 +0300

    feat: Add authentication functionality with sign in and sign up pages

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,35 @@
+import React, { ReactNode, useEffect, useState } from "react";
+import { onAuthStateChanged, getAuth, User } from "firebase/auth";
+import firebase_app from "@/firebase/config";
+
+const auth = getAuth(firebase_app);
+
+export const AuthContext = React.createContext<{ user: User | null }>({
+  user: null,
+});
+
+export const useAuthContext = () => React.useContext(AuthContext);
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {loading ? <div>Loading...</div> : children}
+    </AuthContext.Provider>
+  );
+};
